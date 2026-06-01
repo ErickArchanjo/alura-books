7796a4bcfc01eeae4955736b04e36d706b41f2b9
ErickArchanjo <erick-archanjo@hotmail.com>
2026-06-01 14:35:29 -0300

    commit inicial

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,10 @@
+import livro from '../../imagens/livro.png';
+
+export const livros = [ 
+    {nome: 'O Senhor dos Anéis', autor: 'J.R.R. Tolkien', src: livro, id: 1},
+    {nome: 'A Sociedade do Anel', autor: 'J.R.R. Tolkien', src: livro, id: 2},
+    {nome: 'As Duas Torres', autor: 'J.R.R. Tolkien', src: livro, id: 3},
+    {nome: 'O Retorno do Rei', autor: 'J.R.R. Tolkien', src: livro, id: 4}
+
+
+]
